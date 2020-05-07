36f6e6f8a84541aae1e247ad21bf66c048be0e3d
domainio <omega@omegas-MacBook-Pro.local>
2020-05-07 14:02:06 +0300

    Fetching Next Page of Stories

--- pages/index.js
+++ pages/index.js
@@ -2,29 +2,38 @@ import fetch from 'isomorphic-fetch';
 import Error from 'next/error';
 import StoryList from '../componentns/StoryList';
 import Layout from '../componentns/Layout';
+import Link from 'next/link';
 
-const Index = ({ stories }) => {
-  console.log(stories);
+const Index = ({ stories, page }) => {
+  // console.log(stories);
   if (!stories || stories.length === 0) {
     return <Error statusCode={503} />
   }
   return (
     <Layout title="Hacker Next" description="A Hacker News clone made with Next.js">
       <StoryList stories={stories} />
+      <footer>
+        Page ({page}) > 
+        <Link href={`/?page=${page}`}>
+          <a> Next Page ({page + 1})</a>
+        </Link>
+      </footer>
     </Layout>
   )
 }
 
-Index.getInitialProps = async () => {
+Index.getInitialProps = async ({ req, res, query }) => {
+  console.log('query: ', query);
+  let page = Number(query.page) || 1;
   let stories;
   try {
-    const res = await fetch('https://api.hnpwa.com/v0/news/1.json')
+    const res = await fetch(`https://api.hnpwa.com/v0/news/${page}.json`)
     stories = await res.json();
   } catch (err) {
     console.log(err);
     stories = [];
   }
-  return { stories };
+  return { stories, page };
 }
 
 export default Index;
