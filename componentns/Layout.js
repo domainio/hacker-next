be67b01e38d3e68077e76205cf7fb507a2eb1225
domainio <omega@omegas-MacBook-Pro.local>
2020-05-07 13:26:12 +0300

    Adding Layout Component

---
+++ componentns/Layout.js
@@ -0,0 +1,44 @@
+import Link from 'next/link';
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <div className="container">
+        <nav>
+          <Link href="/">
+            <a>
+              <span className="main-title">Hacker Next</span>
+            </a>
+          </Link>
+        </nav>
+        {children}
+      </div>
+      <style jsx>{`
+        .container {
+          max-width: 800px;
+          margin: 0 auto;
+          background: #f6f6ef;
+        }
+        nav {
+          background: #f60;
+          padding: 1em;
+        }
+        nav > * {
+          display: inline-bloack;
+          color: black;
+        }
+        nav .main-title {
+          font-weight: bold;
+        }
+      `}</style>
+      <style global jsx>{`
+        body {
+          background: white;
+          font-family: Veranda, Geneva, sans-serif;
+        }
+      `}</style>
+    </div>
+  )
+}
+
+export default Layout;
